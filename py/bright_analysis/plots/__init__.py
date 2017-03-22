60efd795fa9fe27abd25d40baf0575f54e8e1366
Andrew Cooper <a.p.cooper@durham.ac.uk>
2017-03-22 12:30:22 +0000

    Adds plots package

---
+++ py/bright_analysis/plots/__init__.py
@@ -0,0 +1,15 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# -*- coding: utf-8 -*-
+"""
+======================
+bright_analysis.sweeps
+======================
+
+Package to handle reduction of survey simulation outputs to more manageable
+'sweep' files. 
+
+.. _DESI: http://desi.lbl.gov
+"""
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
